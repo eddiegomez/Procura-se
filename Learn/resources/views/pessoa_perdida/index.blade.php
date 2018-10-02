fc3bbeec185e0e929be60090f28bef338f3c55e3
Edson Gomes <Edsongomex@gmail.com>
2018-10-02 07:23:30 -0700

    Listar imagens

--- Learn/resources/views/pessoa_perdida/index.blade.php
+++ Learn/resources/views/pessoa_perdida/index.blade.php
@@ -8,7 +8,7 @@
 	@foreach($pessoa_perdida as $p_perdida)
 	<div class="col-lg-3">
 			<div class="card" style="width: 18rem">
-				<img class="card-img-top" src="imgs_p_perdidas/{{$p_perdida->foto}}">
+				<img class="card-img-top" src="/imgs_p_perdidas/{{$p_perdida->foto}}">
 				<div class="card-body">
 
 					<h3 class="card-title">{{$p_perdida->nome}}</h3>
