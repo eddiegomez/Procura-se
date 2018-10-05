0dfb8ebb8acac1105c8c0056d3b3f6ba8cd3163e
JosephMboane <31317713+JosephMboane@users.noreply.github.com>
2018-10-05 12:42:46 +0200

    Revert "Teste"
    This reverts commit 6a6ca1370337052433557892ab0325d9058309e7.

--- Learn/app/Pessoa_perdida.php
+++ Learn/app/Pessoa_perdida.php
@@ -6,7 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pessoa_perdida extends Model
 {
-    public $primaryKey='id_pessoa_perdida';
-    public $table ='pessoa_perdida';
-    public $timestamps = false;
+    public $primaryKey='id_p_perdida';
 }
