818698d1247e55bece6b6b7fb123b9e8ff212b37
JosephMboane <31317713+JosephMboane@users.noreply.github.com>
2018-10-16 10:10:26 +0200

    Update Learn/app/Http/Controllers/pessoaPerdidaController.php

--- Learn/app/Http/Controllers/pessoaPerdidaController.php
+++ Learn/app/Http/Controllers/pessoaPerdidaController.php
@@ -42,15 +42,23 @@ class pessoaPerdidaController extends Controller
      */
     public function store(StorePessoaPerdida $request)
     {
+<<<<<<< HEAD
         if ($request->hasFile('foto')){
             $file = $request->file('foto');
             $name = time().$file->getClientOriginalName();
             $file->move(public_path().'/imgs_p_perdidas/',$name);
         }
      
+=======
+    
+       $file = $request->file('foto');//busca a imagem de um input do tipo file
+       $name = time().$file->getClientOriginalName();//concatena a data e a hora actual ao nome da imagem para que n hajam conflitos
+       $file->move(public_path().'/imgs_p_perdidas/',$name);//efectua uma copia da imagem a pasta do projecto     
+>>>>>>> 8a843e429c14dcbea32c64e8729e8532f724e0a7
         $p_perdida = new Pessoa_perdida();
         $p_perdida->nome = $request->input('nome');
         $p_perdida->sexo = $request->input('sexo');
+        $p_perdida->foto = $name;//aqui mando o nome da img a base de dados
         $p_perdida->data_nasc = $request->input('d_nasc');
         $p_perdida->nacionalidade = $request->input('nacionalidade');
         $p_perdida->naturalidade = $request->input('naturalidade');
